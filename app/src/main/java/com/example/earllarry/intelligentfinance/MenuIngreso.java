5abc4c7628247bc0b38e74a4829d03bae50336cd
labuyot <larryabuyot01@hotmail.com>
2016-02-16 19:06:50 -0400

    boton back
    boton back en menus

--- app/src/main/java/com/example/earllarry/intelligentfinance/MenuIngreso.java
+++ app/src/main/java/com/example/earllarry/intelligentfinance/MenuIngreso.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -17,6 +18,17 @@ public class MenuIngreso extends AppCompatActivity {
         setContentView(R.layout.activity_menu_ingreso);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white); // Set the icon
+
+        // Icon click listener
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("cek", "home selected");
+                startActivity(new Intent(MenuIngreso.this, DashboardDrawer.class));
+            }
+        });
 
         ImageView imageAgregar = (ImageView)findViewById(R.id.imageViewAgregarIngreso);
         ImageView imageInforme = (ImageView)findViewById(R.id.imageViewInformeIngreso);
--- app/src/main/java/com/example/earllarry/intelligentfinance/MenuTarjetas.java
+++ app/src/main/java/com/example/earllarry/intelligentfinance/MenuTarjetas.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -17,6 +18,17 @@ public class MenuTarjetas extends AppCompatActivity {
         setContentView(R.layout.activity_menu_tarjetas);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white); // Set the icon
+
+        // Icon click listener
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("cek", "home selected");
+                startActivity(new Intent(MenuTarjetas.this, DashboardDrawer.class));
+            }
+        });
 
         ImageView imageAgregar = (ImageView)findViewById(R.id.imageViewAgregarTarjeta);
         ImageView imageInforme = (ImageView)findViewById(R.id.imageViewInformeTarjeta);
